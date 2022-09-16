2979280c9f3b95d5f73ef42e0a0f08f628337fcf
Gabriel <bielsantos200510@gmail.com>
2022-09-16 18:16:03 -0300

    Segunda Carga

--- site/src/Cadastro/index.js
+++ site/src/Cadastro/index.js
@@ -1,10 +1,31 @@
+import './index.scss';
+
 export default function Index() {
     return (
-        <div>
-            <div> 
-                <img src="./images/image30.png" />
-                <p> Salvar imagem</p>
+        <div className="Page-Cadastro">
+            <div className="Salvar"> 
+                <input type="file" placeholder='Salvar Imagem' />
+                <img src="./images/image 30.png" />
             </div>
+            <input type="text" placeholder="Nome do produto" />
+            <input type="text" placeholder="Preço" />
+            <input type="text" placeholder="Desconto" />
+            <input type="text" placeholder="Estoque" />
+            <div>
+                    <label>Departamento:</label>
+                    <select>
+                        <option selected disabled hidden>Selecione</option>
+                    </select>
+                </div>
+            <div>
+                    <label>Marca:</label>
+                    <select>
+                        <option selected disabled hidden>Selecione</option>
+                    </select>
+            </div>
+            <textarea placeholder="Descrição do Produto" />
+            
+            <button> Cadastrar </button>
         </div>
     );
 }
