66a2dceed28c0708eb259463adc88b783b72f2ce
Tw0Duck <gusta.ff230@gmail.com>
2022-09-20 16:36:23 -0300

    alterações basicas

--- DB/dml.sql
+++ DB/dml.sql
@@ -1,10 +1,16 @@
+ --inserir produto
+  Insert into tb_produto (nm_produto, vl_preco, qtd_desconto, qtd_estoque, ds_descricao)
+  Values (?,?,?,?,?)
+
+
+ 
  select id_usuario               id,
         nm_usuario             nome
  from   tb_login_usuario    
   join  tb_usuario
     on  tb_login_usuario.id_usuario = tb_usuario.id_usuario
- where  ds_email = ?
-   and  ds_senha = ?
+ where  ds_email =  'admin@gmail.com'
+   and  ds_senha =  '1234'
 
 
-
+--produto
