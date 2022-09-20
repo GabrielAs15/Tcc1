dccb64e2dfea14c5c2ac3a7399519819e4a5e2d6
Tw0Duck <gusta.ff230@gmail.com>
2022-09-20 15:59:44 -0300

    date

--- API/src/repository/connection.js
+++ API/src/repository/connection.js
@@ -1,10 +1,6 @@
 import mysql from 'mysql2/promise.js'
 import 'dotenv/config';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 11f99969081dda3d8563ffc1685081cc0b3b756d
 const con = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
