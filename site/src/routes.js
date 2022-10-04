191f84f8278131d373ba6dd4f17e30703ec9daa1
Tw0Duck <gusta.ff230@gmail.com>
2022-10-04 15:02:07 -0300

    commit terça BuscarProdutos

--- site/src/routes.js
+++ site/src/routes.js
@@ -6,13 +6,16 @@ import Cadastro from './pages/Cadastro/index.js'
 
 import CadastroCliente from './pages/cadastroUsuario/cadastroUsuario.js'
 
+import BuscarProduto from './pages/BuscarProduto/BuscarProdutos.js'
+
 export default function AppRoutes() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/CadastroC" element={<CadastroCliente />} />
+                <Route path="/Cadastro" element={<CadastroCliente />} />
                 <Route path="/Login" element={<Login />} />
-                <Route path="/Cadastro" element={<Cadastro />} />
+                <Route path="/CadastroProduto" element={<Cadastro />} />
+                <Route path='/BuscarProduto' element={<BuscarProduto />} />
             </Routes>
         </BrowserRouter>
     );
