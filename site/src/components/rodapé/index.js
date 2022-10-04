68fbc6f2a05c13541d64a9b0cbb525800874597c
GabrielAs15 <bielsantos200510@gmail.com>
2022-10-04 16:36:08 -0300

    commit rodapé

---
+++ site/src/components/rodapé/index.js
@@ -0,0 +1,43 @@
+export default function Index(){
+    return(
+        <div>
+            <div className="coluna1">
+                <div> Departamentos </div>
+                <div> Computadores </div>
+                <div> Periféricos </div>
+                <div> Consoles</div>
+                <div> Gabinetes </div>
+                <div> Monitores </div>
+                <div> Portáteis </div>
+            </div>
+            <div className="coluna2">
+                <div>Institucional </div>
+                <div> Quem Somos </div>
+                <div> Localização </div>
+            </div>
+            <div className="coluna3">
+                <div> Conta </div>
+                <div> Minha Conta</div>
+                <div> Administrativo</div>
+            </div>
+            <div className="coluna4">
+                <div> Contato </div>
+                <div> </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="parcerias">
+                <img />
+                <img />
+            </div>
+            <div className="contatos">
+                <img />
+                <img />
+                <img />
+                <img />
+            </div>
+        </div>
+    )
+
+}
