1c95f9941ef927223fe8b3b060ee81934f988b60
GabrielAs15 <bielsantos200510@gmail.com>
2022-10-24 16:42:13 -0300

    commit segunda

---
+++ site/src/pages/MeusDados/index.js
@@ -0,0 +1,11 @@
+import Cabecalho from '../../components/cabecalhoUSUARIO/index.js'
+
+import './index.scss'
+
+export default function Index(){
+    return(
+        <div>
+            <Cabecalho />
+        </div>
+    )
+}
