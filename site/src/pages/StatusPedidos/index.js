ca7926c40130809738be67a6f6d65d8b42cefa34
GabrielAs15 <bielsantos200510@gmail.com>
2022-11-01 16:34:47 -0300

    page status pedido 01/11

--- site/src/pages/StatusPedidos/index.js
+++ site/src/pages/StatusPedidos/index.js
@@ -12,13 +12,44 @@ export default function index(){
                     <h2> Nome do cliente: {} </h2>
                     <h2> Produto: {}</h2>
                 </div>
-                <div>
+                <div className="sub-1">
                     <h2> Forma de pagamento: {} </h2>
                     <h2> Endereço: {} </h2>
                 </div>
+
             </div>
             <div>
                 <h1> Status Pedido </h1>
+                <div className="section-2">
+                    <div className="divis-1">
+                        <img src="../images/iconeplaqueta.png" />
+                        <div className="div-1">
+                            <input type="checkbox"/>
+                            <p>Revisado</p>
+                        </div>
+                    </div>
+                    <div className="divis-2">
+                        <img src="../images/image 45.png" />
+                        <div className="div-2">
+                            <input type="checkbox"/>
+                            <p>Separado</p>
+                        </div>
+                    </div>
+                    <div className="divis-3">
+                        <img src="../images/image 46.png" />
+                        <div className="div-3">
+                            <input type="checkbox"/>
+                            <p>À caminho</p>
+                        </div>
+                    </div>
+                    <div className="divis-4">
+                        <img src="../images/image 47.png" />
+                        <div className="div-4">
+                            <input type="checkbox"/>
+                            <p>Entregue</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
